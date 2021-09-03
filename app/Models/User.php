d61fcb84660f04a9e3b74c90ce95ece797b78c87
syaflishovin <syaflishovin@gmail.com>
2021-09-03 13:26:52 +0700

    Added new changes to create User Login Logout

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,16 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -24,7 +24,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
@@ -34,11 +34,24 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
+    protected $casts = ['email_verified_at' => 'datetime'];
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+
+    }//end getJWTIdentifier()
+
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+
+    }//end getJWTCustomClaims()
+
+}//end class 
