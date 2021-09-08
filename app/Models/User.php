22577bb092ec1a306c2c0c49ce9852dee5a32a67
Adhitya Pasande <71677606+tayaanakbaik@users.noreply.github.com>
2021-09-09 01:28:53 +0800

    Revised

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array
      */
-    protected $casts = ['email_verified_at' => 'datetime'];
+    protected $casts = ['email_verified_at' => 'datetime','is_admin' => 'boolean'];
 
 
     public function getJWTIdentifier()
